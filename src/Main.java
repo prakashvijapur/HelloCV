f156bf74b547800a631d0ed93f83d96355a67305
Prakash Vijapur <prakash.vijapur@gmail.com>
2017-12-28 14:41:45 +0530

    test

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,13 @@ import org.opencv.core.Mat;
 public class Main {
 
     public static void main(String[] args) {
+    	 //testing the git
+    	
         System.out.println("Welcome to OpenCV " + Core.VERSION);
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         Mat m  = Mat.eye(3, 3, CvType.CV_8UC1);
         System.out.println("m = " + m.dump());
+       
     }
 
 }
